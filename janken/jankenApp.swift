9458850aa03c9906d440a70b7e58a0080e809060
BurnerD750 <t.chibana54@gmail.com>
2024-02-23 11:30:08 +0900

    Initial Commit

---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by Bana on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
